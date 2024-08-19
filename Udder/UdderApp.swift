d1d7abddab1b8f3311f8e4866b2b2900036908a8
suhacann <suhacan@yahoo.com>
2024-08-19 10:31:05 -0700

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Suha Can on 19.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
